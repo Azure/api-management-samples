67b1dbedcffa76dc6a7f4fd0c752a1f912d6cac0
Samir Solanki <samirsolanki@outlook.com>
2020-10-21 20:59:22 -0700

    comments and option for environments

--- scripts/Set-AzApiManagementClassicVnet.ps1
+++ scripts/Set-AzApiManagementClassicVnet.ps1
@@ -1,6 +1,7 @@
 param
 (
     [Parameter(Mandatory=$true)]
+    [ValidateSet("AzureCloud", "AzureChinaCloud", "AzureUSGovernment", "AzureGermanCloud", "AzureUSSec", "AzureUSNat")]
     [string]$Environment,
 
     [Parameter(Mandatory = $True)]
@@ -23,17 +24,27 @@ param
 #apiversion
 $apiVersion = "2019-12-01"
 
+Write-Host "Connecting to " $Environment
 Connect-AzAccount -Environment $Environment 
 
 # switch to subscription
+Write-Host "Switching to subscription " $SubscriptionId
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
 # get the apim resource
+Write-Host "Fetching Api Management resource " $ServiceName
 $apimResource = Get-AzResource -ResourceType "microsoft.apimanagement/service" -ResourceGroupName $ResourceGroup -ResourceName $ServiceName -ApiVersion $apiVersion
 
 # update capacity
+Write-Host "Updating Capacity from " $apimResource.Sku.Capacity " to " $Capacity
 $apimResource.Sku.Capacity =$Capacity
 
 # Execute the operation
 $apimResource | Set-AzResource -Force 
+
+Write-Host "Update Completed" 
+# get the apim resource
+$apimResource = Get-AzResource -ResourceType "microsoft.apimanagement/service" -ResourceGroupName $ResourceGroup -ResourceName $ServiceName -ApiVersion $apiVersion
+
+Write-Host "New Capacity: " $apimResource.Sku.Capacity
 
