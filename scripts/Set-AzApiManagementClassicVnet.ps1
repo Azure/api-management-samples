e5f096c7ec4dbf997a39c14e47f8b1de7aa2a5c4
Samir Solanki <samirsolanki@outlook.com>
2020-10-21 20:31:30 -0700

    add environment support

---
+++ scripts/Set-AzApiManagementClassicVnet.ps1
@@ -0,0 +1,39 @@
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$environment,
+
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $SubscriptionId,
+
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $ResourceGroup,
+
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $ServiceName,
+
+    [Parameter(Mandatory = $True)]
+    [System.Int]
+    $capacity
+)
+
+#apiversion
+$apiVersion = "2019-12-01"
+
+Connect-AzAccount -Environment $environment 
+
+# switch to subscription
+Select-AzSubscription -SubscriptionId $SubscriptionId
+
+# get the apim resource
+$apimResource = Get-AzResource -ResourceType "microsoft.apimanagement/service" -ResourceGroupName $ResourceGroup -ResourceName $ServiceName -ApiVersion $apiVersion
+
+# update capacity
+$apimold.Sku.Capacity =$capacity
+
+# Execute the operation
+$apimResource | Set-AzResource -Force 
+
