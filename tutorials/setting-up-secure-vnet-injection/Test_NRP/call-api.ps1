deec77b0d0699419bc0fbb782d6fc4833077bcce
Samir Solanki <samirsolanki@outlook.com>
2021-08-23 18:17:42 -0700

    demo setup for APIM in Forced Tunneled VNET (#47)

---
+++ tutorials/setting-up-secure-vnet-injection/Test_NRP/call-api.ps1
@@ -0,0 +1,29 @@
+﻿param(
+    [String]$apimServiceId,
+    [String]$azureManagementUrl = "https://management.azure.com",
+    [String]$apiVersion = "2021-04-01-preview"
+)
+
+function Get-OutboundNetworkDependenciesAPI {
+    $azContext = Get-AzContext
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+    $token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+    $authHeader = @{
+        'Content-Type'='application/json'
+        'Authorization'='Bearer ' + $token.AccessToken
+    }
+
+    # Invoke the REST API
+    $restUri = "$($azureManagementUrl)$($apimServiceId)/OutboundNetworkDependenciesEndpoints/?api-version=$($apiVersion)"
+
+    Write-Host "ReqUri : " $restUri
+
+    $response = Invoke-RestMethod -Uri $restUri -Method Get -Headers $authHeader
+
+    Write-Host $response.value
+    return $response
+    
+}
+
+return Get-OutboundNetworkDependenciesAPI
